c9ce77b04404c0da3747ce20bf42f17fb3e98e0f
Riya Vatyani <riyavatyani15@gmail.com>
2025-12-25 02:31:38 +0530

    Fix aiRoutes import to use generateAI

--- backend/routes/aiRoutes.js
+++ backend/routes/aiRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const { protect } = require("../middlewares/authMiddleware");
-const { generateResume } = require("../controllers/aiController");
+const { generateAI } = require("../controllers/aiController");
 
-router.post("/generate", protect, generateResume);
+console.log("AI ROUTE LOADED, generateAI =", generateAI);
+
+router.post("/generate", protect, generateAI);
 
 module.exports = router;
