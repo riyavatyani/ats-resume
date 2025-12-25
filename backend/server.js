809234cf5e5a9728f4d1ba1aa957cec887ed8317
Riya Vatyani <riyavatyani15@gmail.com>
2025-12-25 19:41:21 +0530

    fix: express fallback route for node 22

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ app.use(
 );
 
 // React fallback (VERY IMPORTANT)
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(
     path.join(__dirname, "../frontend/dist/index.html")
   );
