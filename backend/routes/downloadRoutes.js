3e0a63c37796b17b1609b56a76f57d3e07615028
Riya Vatyani <riyavatyani15@gmail.com>
2025-12-25 21:22:52 +0530

    fix: remove invalid openai parser import breaking express router

--- backend/routes/downloadRoutes.js
+++ backend/routes/downloadRoutes.js
@@ -3,8 +3,6 @@ const router = express.Router();
 
 const { protect } = require("../middlewares/authMiddleware");
 const { checkDownload } = require("../controllers/downloadController");
-console.log("protect =", protect);
-console.log("checkDownload =", checkDownload);
 
 router.post("/resume", protect, checkDownload);
 
